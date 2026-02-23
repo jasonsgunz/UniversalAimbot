0814acfedec5061c1d6ae4fe528c4380ffbbb6b9
jasonsgunz <jasonmcmini@gmail.com>
2026-02-23 16:50:32 +0100

    UniversalAimbot V3.3.1

--- UniversalAimbot.lua
+++ UniversalAimbot.lua
@@ -508,9 +508,9 @@ LocalPlayer.CharacterAdded:Connect(function() selfOptions.fly.enabled = false; t
 
 pcall(function()
     game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "VERSION V.3.2",
+        Title = "VERSION V.3.3",
         Text = "This Script was made by jasonsgunz on Github.",
-        Icon = "rbxthumb://type=Asset&id=12645430259&w=150&h=150",
+        Icon = "rbxthumb://type=Asset&id=15772199412&w=150&h=150",
         Duration = 6
     })
 end)
